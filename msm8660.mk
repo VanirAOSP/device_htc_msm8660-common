e31bdef7f88de1f9720fa7fe35dbae4e00759070
Chad Froebel <chad0989@rochester.rr.com>
2013-02-05 19:49:57 -0500

    Remove unwanted script

--- msm8660.mk
+++ msm8660.mk
@@ -14,7 +14,6 @@
 
 # Qualcomm scripts
 PRODUCT_COPY_FILES += \
-    device/htc/msm8660-common/prebuilt/89bruce:system/etc/init.d/89bruce \
     device/htc/msm8660-common/prebuilt/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh \
     device/htc/msm8660-common/prebuilt/init.qcom.efs.sync.sh:system/etc/init.qcom.efs.sync.sh
 
