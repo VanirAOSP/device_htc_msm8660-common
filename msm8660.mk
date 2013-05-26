b2a79c89c17c5ed82943f09ea00815188b5eaa9f
David Hays <dhays90@gmail.com>
2013-05-25 20:46:38 -0500

    configs: removing unneeded audio_policy.conf
    swapped new audio-caf msm8660 works perfectly with VOIP

--- msm8660.mk
+++ msm8660.mk
@@ -44,6 +44,7 @@ PRODUCT_PACKAGES += \
     audio_policy.msm8660 \
     audio.primary.msm8660 \
     libaudioutils \
+    audio_policy.conf
 
 # Camera wrapper
 PRODUCT_PACKAGES += \
@@ -92,10 +93,6 @@ PRODUCT_PACKAGES += \
     make_ext4fs \
     setup_fs
 
-# Media configuration
-PRODUCT_COPY_FILES += \
-    device/htc/msm8660-common/configs/audio_policy.conf:system/etc/audio_policy.conf
-	
 # MSM8660 firmware
 PRODUCT_COPY_FILES += \
     device/htc/msm8660-common/firmware/vidc_1080p.fw:system/etc/firmware/vidc_1080p.fw \
