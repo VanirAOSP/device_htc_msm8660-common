534eaab2de5bc0f7533193e7a04ddf7817fb03d1
David Hays <dhays90@gmail.com>
2013-06-02 00:57:28 -0500

    Revert "msm8660: Fix mic in Hangouts"
    This reverts commit 902cd8708d55083c88bad705bee5d7ffc062776b.

--- msm8660.mk
+++ msm8660.mk
@@ -43,7 +43,8 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio_policy.msm8660 \
     audio.primary.msm8660 \
-    libaudioutils
+    libaudioutils \
+    audio_policy.conf
 
 # Camera wrapper
 PRODUCT_PACKAGES += \
@@ -91,10 +92,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     make_ext4fs \
     setup_fs
-
-# Media configuration
-PRODUCT_COPY_FILES += \
-    device/htc/msm8660-common/configs/audio_policy.conf:system/etc/audio_policy.conf
 
 # MSM8660 firmware
 PRODUCT_COPY_FILES += \
