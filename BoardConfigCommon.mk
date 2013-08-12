ff8645c11ded1db9d23283bd8db28b8943a7cb1f
Darescu Ionut <johnny@esi.ro>
2013-08-12 11:33:40 +0300

    msm8660: Basic SELinux policy
    *copy-paste from samsung msm8660
    *contains basic htc support
    
    Change-Id: Iee99b74ebdc9e1295d7f292668ac2d0c8e28d2b7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -79,6 +79,39 @@ BOARD_VOLD_MAX_PARTITIONS := 36
 BOARD_USES_QCOM_GPS := true
 BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
 
+# SELinux
+BOARD_SEPOLICY_DIRS += \
+    device/htc/msm8660-common/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file_contexts \
+    property_contexts \
+    te_macros \
+    bridge.te \
+    camera.te \
+    conn_init.te \
+    device.te \
+    dhcp.te \
+    domain.te \
+    drmserver.te \
+    file.te \
+    kickstart.te \
+    init.te \
+    mediaserver.te \
+    mpdecision.te \
+    netmgrd.te \
+    property.te \
+    qmux.te \
+    rild.te \
+    rmt.te \
+    sensors.te \
+    surfaceflinger.te \
+    system.te \
+    tee.te \
+    thermald.te \
+    ueventd.te \
+    wpa_supplicant.te
+
 # Graphics
 COMMON_GLOBAL_CFLAGS += -DQCOM_NO_SECURE_PLAYBACK -DREFRESH_RATE=60 -DHTC_RGBA_8888_OFFSET
 USE_OPENGL_RENDERER := true
